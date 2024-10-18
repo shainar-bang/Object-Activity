8228f448cb4f4ba3aef67e367f8866866a88975f
shainar-bang <rabangshaina@gmail.com>
2024-10-18 18:55:37 +0800

    Create objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Subaru",
+    model: "Crosstrek",
+    color: "Grey"
+};
+
+console.log(typeof car);
+
+car.type = "Lamborghini";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
